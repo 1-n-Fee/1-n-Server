6f77cf4dc2658fd0dc95c601621ff5caa4b4bc60
seungtaekhong <hsk4991149@naver.com>
2022-08-10 20:56:13 +0900

    feat: DB 테이블을 볼 수 있는 페이지 추가(/table)

---
+++ 1-nServer/src/main/java/konkuk/nServer/domain/common/controller/DbTableController.java
@@ -0,0 +1,79 @@
+package konkuk.nServer.domain.common.controller;
+
+import konkuk.nServer.domain.post.domain.Comment;
+import konkuk.nServer.domain.post.domain.Post;
+import konkuk.nServer.domain.post.repository.CommentRepository;
+import konkuk.nServer.domain.post.repository.PostRepository;
+import konkuk.nServer.domain.proposal.domain.Proposal;
+import konkuk.nServer.domain.proposal.domain.ProposalDetail;
+import konkuk.nServer.domain.proposal.repository.ProposalDetailRepository;
+import konkuk.nServer.domain.proposal.repository.ProposalRepository;
+import konkuk.nServer.domain.store.domain.Menu;
+import konkuk.nServer.domain.store.domain.Store;
+import konkuk.nServer.domain.store.repository.MenuRepository;
+import konkuk.nServer.domain.store.repository.StoreRepository;
+import konkuk.nServer.domain.user.domain.*;
+import konkuk.nServer.domain.user.repository.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class DbTableController {
+    private final KakaoRepository kakaoRepository;
+    private final NaverRepository naverRepository;
+    private final GoogleRepository googleRepository;
+    private final PasswordRepository passwordRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final StoremanagerRepository storemanagerRepository;
+    private final ProposalRepository proposalRepository;
+    private final ProposalDetailRepository proposalDetailRepository;
+    private final StoreRepository storeRepository;
+    private final CommentRepository commentRepository;
+    private final MenuRepository menuRepository;
+
+    @GetMapping("/table")
+    public String showDbTable(Model model) {
+        log.info("DB 테이블 보기");
+        List<Kakao> kakaos = kakaoRepository.findAll();
+        List<Naver> navers = naverRepository.findAll();
+        List<Google> googles = googleRepository.findAll();
+        List<Password> passwords = passwordRepository.findAll();
+        List<User> users = userRepository.findAll();
+        List<Post> posts = postRepository.findAll();
+        List<Storemanager> storemanagers = storemanagerRepository.findAll();
+        List<Proposal> proposals = proposalRepository.findAll();
+        List<ProposalDetail> proposalDetails = proposalDetailRepository.findAll();
+        List<Store> stores = storeRepository.findAll();
+        List<Comment> comments = commentRepository.findAll();
+        List<Menu> menus = menuRepository.findAll();
+
+
+        model.addAttribute("kakaos", kakaos);
+        model.addAttribute("navers", navers);
+        model.addAttribute("googles", googles);
+        model.addAttribute("passwords", passwords);
+        model.addAttribute("users", users);
+        model.addAttribute("posts", posts);
+        model.addAttribute("storemanagers", storemanagers);
+        model.addAttribute("proposals", proposals);
+        model.addAttribute("proposalDetails", proposalDetails);
+        model.addAttribute("stores", stores);
+        model.addAttribute("comments", comments);
+        model.addAttribute("menus", menus);
+
+        List<String> table = Arrays.asList("kakao", "naver", "google", "password", "user", "storemanager",
+                "post", "proposal", "proposalDetail", "store", "comment", "menu");
+        model.addAttribute("tableName", table);
+
+        return "showDbTable";
+    }
+}
---
+++ 1-nServer/src/main/java/konkuk/nServer/websocket/config/WebSocketConfig.java
@@ -0,0 +1,42 @@
+package konkuk.nServer.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // ws://localhost:8080/ws/chat
+        registry.addEndpoint("/ws/chat") // 웹소켓에 접속하기 위한 endpoint
+                .setAllowedOriginPatterns("*") // CORS 정책
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 메시지 받을 때 관련 경로 설정
+        // "/sub"이  prefix(api 경로 맨 앞)에 붙은 경우, messageBroker가 잡아서 해당 채팅방을 구독하고 있는 클라이언트에게 메시지를 전달해줌
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub"); // 메시지 보낼 때 관련 경로 설정
+        // 클라이언트가 메시지를 보낼 때 경로 맨앞에 "/pub"이 붙어있으면 Broker로 보내짐.
+    }
+
+    // RabbitMQ 설정
+    /*
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub")
+                .enableStompBrokerRelay("/topic")
+                .setRelayHost("localhost")
+                .setVirtualHost("/")
+                .setRelayPort(61613)
+                .setClientLogin("guest")
+                .setClientPasscode("guest");
+    }*/
+}
